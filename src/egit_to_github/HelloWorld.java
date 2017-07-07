eae59d97596ca242ca6035c504b57c7f7904173f
lyons <lyons@lyons-PC>
2017-07-07 12:43:57 +0100

    Added Hello World class

---
+++ src/egit_to_github/HelloWorld.java
@@ -0,0 +1,11 @@
+package egit_to_github;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
